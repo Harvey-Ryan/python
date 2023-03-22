c96c60ed713a5dcf615cfc1f88956520e45beb8a
Harvey-Ryan <124230161+Harvey-Ryan@users.noreply.github.com>
2023-03-22 18:24:37 -0500

    separated modules

--- fundamentals/fundamentals/Bank_Account/server.py
+++ fundamentals/fundamentals/Bank_Account/server.py
@@ -1,44 +1,45 @@
-class BankAccount:
+from models.bank_account import BankAccount;
+# class BankAccount:
 
-    # def __init__(self, int_rate, balance, acct_num):
-    def __init__(self, int_rate, balance):
+#     # def __init__(self, int_rate, balance, acct_num):
+#     def __init__(self, int_rate, balance):
 
-        #self.acct_num = acct_num
-        self.int_rate = int_rate
-        self.balance = balance
+#         #self.acct_num = acct_num
+#         self.int_rate = int_rate
+#         self.balance = balance
 
-    def deposit(self, amount):
-        self.balance = self.balance + amount
-        #print(f"Account: {self.acct_num}")
-        #print(f"Balance: ${self.balance}")
-        return self
+#     def deposit(self, amount):
+#         self.balance = self.balance + amount
+#         #print(f"Account: {self.acct_num}")
+#         #print(f"Balance: ${self.balance}")
+#         return self
     
-    def withdraw(self, amount):
-        self.balance = self.balance - amount
-        #print(f"Account: {self.acct_num}")
-        #print(f"Balance: ${self.balance}")
-        return self
+#     def withdraw(self, amount):
+#         self.balance = self.balance - amount
+#         #print(f"Account: {self.acct_num}")
+#         #print(f"Balance: ${self.balance}")
+#         return self
     
-    def display_account_info(self):
-        #print(f"Account: {self.acct_num}")
-        #print(f"Balance: ${self.balance}")
-        return self
+#     def display_account_info(self):
+#         #print(f"Account: {self.acct_num}")
+#         #print(f"Balance: ${self.balance}")
+#         return self
 
-    def yield_interest(self):
-        if self.balance > 0:
-            self.balance = self.balance + (self.balance * self.int_rate)
-            #print(f"Account: {self.acct_num}")
-            #print(f"Your balance with accumulated interest is: ${self.balance}")
-            return self
-        else:
-            #print(f"Account: {self.acct_num}")
-            #print(f"$0 balance.")
-            return self
+#     def yield_interest(self):
+#         if self.balance > 0:
+#             self.balance = self.balance + (self.balance * self.int_rate)
+#             #print(f"Account: {self.acct_num}")
+#             #print(f"Your balance with accumulated interest is: ${self.balance}")
+#             return self
+#         else:
+#             #print(f"Account: {self.acct_num}")
+#             #print(f"$0 balance.")
+#             return self
         
-    def acct_info(self):
-        print(f"Account Number: {self.acct_num}")
-        print(f"Interest Rate: {self.int_rate}")
-        print(f"Account Balance: {self.balance}")
+#     def acct_info(self):
+#         print(f"Account Number: {self.acct_num}")
+#         print(f"Interest Rate: {self.int_rate}")
+#         print(f"Account Balance: {self.balance}")
 
 class User:
     def __init__(self, name, email):
