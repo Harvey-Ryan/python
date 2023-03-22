35420bbd88aa4b547aa91f2be39cee3d92cc5451
Harvey-Ryan <124230161+Harvey-Ryan@users.noreply.github.com>
2023-03-22 18:26:06 -0500

    attempt 2

---
+++ modularizing/child.py
@@ -0,0 +1,6 @@
+import parent
+
+print(locals())
+# {'__name__': '__main__', '__doc__': None, '__package__': None, '__loader__': <_frozen_importlib_external.SourceFileLoader object at 0x000001DFDEC45890>, '__spec__': None, '__annotations__': {}, '__builtins__': <module 'builtins' (built-in)>, '__file__': 'C:\\Users\\harve\\Desktop\\python\\modularizing\\child.py', '__cached__': None, 'parent': <module 'parent' from 'C:\\Users\\harve\\Desktop\\python\\modularizing\\parent.py'>}
+
+print(__name__)
