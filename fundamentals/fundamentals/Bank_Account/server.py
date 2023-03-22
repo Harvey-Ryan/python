4af5f68c77c937dbdc4c0a7938e375de684823a0
Harvey-Ryan <124230161+Harvey-Ryan@users.noreply.github.com>
2023-03-22 18:33:03 -0500

    cleaned up code

--- fundamentals/fundamentals/Bank_Account/server.py
+++ fundamentals/fundamentals/Bank_Account/server.py
@@ -1,51 +1,9 @@
 from models.bank_account import BankAccount;
-# class BankAccount:
-
-#     # def __init__(self, int_rate, balance, acct_num):
-#     def __init__(self, int_rate, balance):
-
-#         #self.acct_num = acct_num
-#         self.int_rate = int_rate
-#         self.balance = balance
-
-#     def deposit(self, amount):
-#         self.balance = self.balance + amount
-#         #print(f"Account: {self.acct_num}")
-#         #print(f"Balance: ${self.balance}")
-#         return self
-    
-#     def withdraw(self, amount):
-#         self.balance = self.balance - amount
-#         #print(f"Account: {self.acct_num}")
-#         #print(f"Balance: ${self.balance}")
-#         return self
-    
-#     def display_account_info(self):
-#         #print(f"Account: {self.acct_num}")
-#         #print(f"Balance: ${self.balance}")
-#         return self
-
-#     def yield_interest(self):
-#         if self.balance > 0:
-#             self.balance = self.balance + (self.balance * self.int_rate)
-#             #print(f"Account: {self.acct_num}")
-#             #print(f"Your balance with accumulated interest is: ${self.balance}")
-#             return self
-#         else:
-#             #print(f"Account: {self.acct_num}")
-#             #print(f"$0 balance.")
-#             return self
-        
-#     def acct_info(self):
-#         print(f"Account Number: {self.acct_num}")
-#         print(f"Interest Rate: {self.int_rate}")
-#         print(f"Account Balance: {self.balance}")
 
 class User:
     def __init__(self, name, email):
         self.name = name
         self.email = email
-        # self.account = BankAccount(int_rate=0.02, balance=0)
         self.accounts = {}
         self.acc_name = ""
 
@@ -56,15 +14,11 @@ class User:
 
     def make_deposit(self, amount, type):
         self.accounts[type].deposit(amount)
-        # self.accounts[type].balance = self.accounts[type].balance + amount
-        #print(f"Account: {self.acct_num}")
         print(f"{self.acc_name} Balance: ${self.accounts[type].balance}")
         return self
     
     def make_withdraw(self, amount, type):
         self.accounts[type].withdraw(amount)
-        #self.accounts[type].balance = self.accounts[type].balance - amount
-        #print(f"Account: {self.acct_num}")
         print(f"{self.acc_name} Balance: ${self.accounts[type].balance}")
         return self
     
@@ -114,12 +68,3 @@ Christian_T.new_acct(.05, 250, "Checking")
 Ryan_H.make_deposit(53, "Checking").make_withdraw(10, "Checking")
 Ryan_H.transfer(Christian_T, "Checking", 100, 100)
 Ryan_H.display_account_info()
-
-
-
-# acct_1 = BankAccount(.5, 100.01, 9998)
-# acct_1.deposit(25.03).deposit(25.03).deposit(25.03).yield_interest().display_account_info()
-# acct_2 = BankAccount(.25, 200.01, 9997)
-# acct_2.deposit(25.03).deposit(25.03).withdraw(25.03).withdraw(25.03).withdraw(25.03).withdraw(25.03).yield_interest().display_account_info()
-# acct_1.acct_info()
-# acct_2.acct_info()
