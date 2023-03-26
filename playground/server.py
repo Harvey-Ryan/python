5e9869915a2a0b9c19087a1402b9b736f3061652
Harvey-Ryan <124230161+Harvey-Ryan@users.noreply.github.com>
2023-03-26 14:09:25 -0500

    Commented logic.

--- playground/server.py
+++ playground/server.py
@@ -4,19 +4,19 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'PLAYGROUND'
+    return 'PLAYGROUND' #Renders nothing but a title.
 
 @app.route('/play')
 def play():
-    return render_template('index.html', color='#9FC5F8')
+    return render_template('index.html', color='#9FC5F8') #Renders default blue 3 box.
 
 @app.route('/play/<int:num>')
 def box_times(num):
-    return render_template('index.html', num=num, color='#9FC5F8')
+    return render_template('index.html', num=num, color='#9FC5F8') #Renders blue boxes x times.
 
 @app.route('/play/<int:num>/<string:color>')
 def color_of_boxes(num, color='blue'):
-    return render_template('index.html', num=num, color=color)
+    return render_template('index.html', num=num, color=color) #Renders x boxes in color of choice.
 
 if __name__ == '__main__':
     app.run(debug=True)
