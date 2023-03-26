de3af6508932d9f6058498ff17e6c6f32804d386
Harvey-Ryan <124230161+Harvey-Ryan@users.noreply.github.com>
2023-03-26 13:59:48 -0500

    Playground

--- fundamentals/fundamentals/W3D2OH/server.py
+++ fundamentals/fundamentals/W3D2OH/server.py
@@ -1,10 +1,10 @@
-from flask import Flask,render_template
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
-def index():
-    return render_template("index.html")
+@app.route("/<string:name>/<int:number>")
+def index(name,number):
+    return render_template("index.html", name=name, number=number)
 
 @app.route("/coding/dojo/rules")
 def hello():
